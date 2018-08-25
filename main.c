d2f12706457b6fcf9b4a57568bf1f174b6c35cc0
profesor <profesor@LAB00PC00>
2018-08-25 13:51:14 -0300

    cmbio en el main

--- main.c
+++ main.c
@@ -6,12 +6,7 @@ int main()
     int edad, i;
     int seguir = 1;
 
-    /*for(i=0; i<3; i++)
-    {
-        printf("Ingrese su edad:");
-        scanf("%d",&edad);
-        printf("Su edad es: %d",edad);
-    }*/
+    
     i=0;
 
     while(i<10 || seguir)
