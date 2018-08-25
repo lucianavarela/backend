3f5d8f2773ed26cff874660470da09c54ffb7fa1
profesor <profesor@LAB00PC00>
2018-08-25 13:28:09 -0300

    Inicio de trabajo practico

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int edad, i;
+    int seguir = 1;
+
+    /*for(i=0; i<3; i++)
+    {
+        printf("Ingrese su edad:");
+        scanf("%d",&edad);
+        printf("Su edad es: %d",edad);
+    }*/
+    i=0;
+
+    while(i<10 || seguir)
+    {
+        printf("Ingrese su edad:");
+        scanf("%d",&edad);
+        printf("Su edad es: %d",edad);
+        i++
+    }
+
+    return 0;
+}
+
+        printf("\nDesea continuar? 1/0")
+        scanf("%d",&seguir);
